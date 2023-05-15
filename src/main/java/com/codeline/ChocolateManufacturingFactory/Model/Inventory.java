7ea6989e4915ceeae05250684120d8cf7bb0347b
R4wdha19 <rweedh@hotmail.com>
2023-05-15 07:14:12 -0700

    Added To Model

--- src/main/java/com/codeline/ChocolateManufacturingFactory/Model/Inventory.java
+++ src/main/java/com/codeline/ChocolateManufacturingFactory/Model/Inventory.java
@@ -1,7 +1,23 @@
 package com.codeline.ChocolateManufacturingFactory.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+@Data
+@Setter
+@Getter
+@Entity
 public class Inventory {
-    location;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer inventoryId;
+    String inventoryLocation;
+    List<String> inventoryproductList;
+    String inventoryManagedBy;
+
 
 }
--- src/main/java/com/codeline/ChocolateManufacturingFactory/Model/Order.java
+++ src/main/java/com/codeline/ChocolateManufacturingFactory/Model/Order.java
@@ -1,4 +1,16 @@
 package com.codeline.ChocolateManufacturingFactory.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Data
+@Setter
+@Getter
+@Entity
 public class Order {
+    Integer orderId;
+    String
 }
