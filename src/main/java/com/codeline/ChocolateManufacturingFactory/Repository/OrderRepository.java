c97aa6924b61187ca1f05453033e99d63a668c50
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-05-20 22:58:18 +0400

    Added Queries To Product

--- src/main/java/com/codeline/ChocolateManufacturingFactory/Repository/OrderRepository.java
+++ src/main/java/com/codeline/ChocolateManufacturingFactory/Repository/OrderRepository.java
@@ -16,34 +16,34 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     @Query(value = "select o from Order o where o.id =: orderId")
     Order getOrderById (@Param("orderId") Integer id);
 
-    @Query("select i from Order i")
+    @Query("select o from Order o")
     Order getAllOrders();
 
     @Query(value = "select * from customer_order where created_Date Like Concat (?1,%)", nativeQuery = true)
     List<Order> getAllOrdersByCreatedDate(@Param("createdDate") String createdDate);
 
-    @Query("select i from Order i where i.isActive = 1")
+    @Query("select o from Order o where o.isActive = 1")
     List<Order> getAllActiveOrders();
 
-    @Query("select i from Order i where i.isActive = 0")
+    @Query("select o from Order o where o.isActive = 0")
     List<Order> getAllInActiveOrders();
 
-    @Query(value = "select i from Order i where i.id = (select Max(i.id) from Order i )")
+    @Query(value = "select o from Order o where o.id = (select Max(o.id) from Order o )")
     Order getLatestRow();
 
 
-    @Query(value = "select i from Order i where i.updatedDate = (select Max(i.updatedDate) from Order i)")
+    @Query(value = "select o from Order o where o.updatedDate = (select Max(o.updatedDate) from Order o)")
     Order getLatestUpdated();
 
-    @Query(value = "select i from Order i where i.createdDate > :createdDate")
+    @Query(value = "select o from Order o where o.createdDate > :createdDate")
     List<Order> getAllOrdersCreatedAfterDate(@Param("createdDate") Date createdDate);
 
     @Modifying
     @Transactional
-    @Query(value = "update Order i Set i.isActive = false")
+    @Query(value = "update Order o Set o.isActive = false")
     void deleteAllOrder();
 
-    @Query(value = "select i from Order i where i.createdDate > :createdDate")
+    @Query(value = "select o from Order o where o.createdDate > :createdDate")
     List<Order> deleteAllOrdersCreatedAfterDate(@Param("createdDate") Date createdDate);
 
 }
--- src/main/java/com/codeline/ChocolateManufacturingFactory/Repository/ProductRepository.java
+++ src/main/java/com/codeline/ChocolateManufacturingFactory/Repository/ProductRepository.java
@@ -2,12 +2,48 @@ package com.codeline.ChocolateManufacturingFactory.Repository;
 
 import com.codeline.ChocolateManufacturingFactory.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query(value = "select p from Product p where p.id =:productId")
     Product getProductById(@Param("productId") Integer id);
+
+    @Query("select p from Product p")
+    Product getAllProducts();
+
+    @Query(value = "select * from Product where created_Date Like Concat (?1,%)", nativeQuery = true)
+    List<Product> getAllProductsByCreatedDate(@Param("createdDate") String createdDate);
+
+    @Query("select p from Product p where p.isActive = 1")
+    List<Product> getAllActiveProducts();
+
+    @Query("select p from Product p where p.isActive = 0")
+    List<Product> getAllInActiveProducts();
+
+    @Query(value = "select p from Product p where p.id = (select Max(p.id) from Product p )")
+    Product getLatestRow();
+
+
+    @Query(value = "select p from Product p where p.updatedDate = (select Max(p.updatedDate) from Product p)")
+    Product getLatestUpdated();
+
+    @Query(value = "select p from Product p where p.createdDate > :createdDate")
+    List<Product> getAllProductsCreatedAfterDate(@Param("createdDate") Date createdDate);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update Product p Set p.isActive = false")
+    void deleteAllProduct();
+
+    @Query(value = "select p from Product p where p.createdDate > :createdDate")
+    List<Product> deleteAllProductsCreatedAfterDate(@Param("createdDate") Date createdDate);
+
 }
