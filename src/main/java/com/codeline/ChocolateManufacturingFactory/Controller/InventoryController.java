bda01a76ebbc235e90994ffcdcf8cc13245da51d
R4wdha19 <rweedh@hotmail.com>
2023-05-22 17:09:00 +0400

    Added Security

--- src/main/java/com/codeline/ChocolateManufacturingFactory/Controller/InventoryController.java
+++ src/main/java/com/codeline/ChocolateManufacturingFactory/Controller/InventoryController.java
@@ -1,12 +1,11 @@
 package com.codeline.ChocolateManufacturingFactory.Controller;
 
-import com.codeline.ChocolateManufacturingFactory.Model.Inventory;
 import com.codeline.ChocolateManufacturingFactory.RequestObject.InventoryRequestObject;
 import com.codeline.ChocolateManufacturingFactory.ResponseObject.InventoryResponseObject;
-import com.codeline.ChocolateManufacturingFactory.ResponseObject.ProductInventoryResponse;
 import com.codeline.ChocolateManufacturingFactory.ResponseObject.TrackInventoryResponse;
 import com.codeline.ChocolateManufacturingFactory.Service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -50,24 +49,27 @@ public class InventoryController {
 
     @RequestMapping(value = "getLatestUpdated", method = RequestMethod.GET)
     public InventoryResponseObject getLatestUpdated() {
-        return InventoryResponseObject.convertRequestToResponse(inventoryService.getLatestUpdated()) ;
+        return InventoryResponseObject.convertRequestToResponse(inventoryService.getLatestUpdated());
     }
 
     @RequestMapping(value = "getAllInventoriesByCreatedDate", method = RequestMethod.GET)
     public List<InventoryResponseObject> getAllInventoriesCreatedAfterDate(@RequestParam Date createdDate) {
-        return InventoryResponseObject.convertRequestListToResponseList(inventoryService.getAllInventoriesCreatedAfterDate(createdDate)) ;
+        return InventoryResponseObject.convertRequestListToResponseList(inventoryService.getAllInventoriesCreatedAfterDate(createdDate));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "deleteAllInventories", method = RequestMethod.POST)
     public void deleteAllInventories() {
         inventoryService.deleteAllInventories();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "deleteAllInventoriesCreatedAfterDate", method = RequestMethod.POST)
     public void deleteAllInventoriesCreatedAfterDate(Date createdDate) {
         inventoryService.deleteAllInventoriesCreatedAfterDate(createdDate);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "updateInventory", method = RequestMethod.POST)
     public void updateInventory(InventoryRequestObject inventoryRequestObject) {
         inventoryService.updateInventory(inventoryRequestObject);
