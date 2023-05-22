36dd9266e3ed059b6f224c2b4a2020dd43c590b3
R4wdha19 <rweedh@hotmail.com>
2023-05-22 17:06:24 +0400

    Added Controller To Inventory

--- src/main/java/com/codeline/ChocolateManufacturingFactory/Controller/InventoryController.java
+++ src/main/java/com/codeline/ChocolateManufacturingFactory/Controller/InventoryController.java
@@ -1,12 +1,15 @@
 package com.codeline.ChocolateManufacturingFactory.Controller;
 
+import com.codeline.ChocolateManufacturingFactory.Model.Inventory;
 import com.codeline.ChocolateManufacturingFactory.RequestObject.InventoryRequestObject;
+import com.codeline.ChocolateManufacturingFactory.ResponseObject.InventoryResponseObject;
 import com.codeline.ChocolateManufacturingFactory.ResponseObject.ProductInventoryResponse;
 import com.codeline.ChocolateManufacturingFactory.ResponseObject.TrackInventoryResponse;
 import com.codeline.ChocolateManufacturingFactory.Service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -19,8 +22,54 @@ public class InventoryController {
     public void createInventory(@RequestBody InventoryRequestObject inventoryRequestObject) {
         inventoryService.createInventory(inventoryRequestObject);
     }
+
     @RequestMapping(value = "trackInventories", method = RequestMethod.GET)
-    public List<TrackInventoryResponse> trackInventories(){
+    public List<TrackInventoryResponse> trackInventories() {
         return inventoryService.trackInventoryLevels();
+    }
+
+    @RequestMapping(value = "getAllInventories", method = RequestMethod.GET)
+    public List<InventoryResponseObject> getAllInventories() {
+        return InventoryResponseObject.convertRequestListToResponseList(inventoryService.getAllInventories());
+    }
+
+    @RequestMapping(value = "getAllInventoriesByCreatedDate", method = RequestMethod.GET)
+    public List<InventoryResponseObject> getAllInventoriesByCreatedDate(@RequestParam String createdDate) {
+        return InventoryResponseObject.convertRequestListToResponseList(inventoryService.getAllInventoriesByCreatedDate(createdDate));
+    }
+
+    @RequestMapping(value = "getAllInActiveInventories", method = RequestMethod.GET)
+    public List<InventoryResponseObject> getAllInActiveInventories() {
+        return InventoryResponseObject.convertRequestListToResponseList(inventoryService.getAllInActiveInventories());
+    }
+
+    @RequestMapping(value = "getLatestRow", method = RequestMethod.GET)
+    public InventoryResponseObject getLatestRow() {
+        return InventoryResponseObject.convertRequestToResponse(inventoryService.getLatestRow());
+    }
+
+    @RequestMapping(value = "getLatestUpdated", method = RequestMethod.GET)
+    public InventoryResponseObject getLatestUpdated() {
+        return InventoryResponseObject.convertRequestToResponse(inventoryService.getLatestUpdated()) ;
+    }
+
+    @RequestMapping(value = "getAllInventoriesByCreatedDate", method = RequestMethod.GET)
+    public List<InventoryResponseObject> getAllInventoriesCreatedAfterDate(@RequestParam Date createdDate) {
+        return InventoryResponseObject.convertRequestListToResponseList(inventoryService.getAllInventoriesCreatedAfterDate(createdDate)) ;
+    }
+
+    @RequestMapping(value = "deleteAllInventories", method = RequestMethod.POST)
+    public void deleteAllInventories() {
+        inventoryService.deleteAllInventories();
+    }
+
+    @RequestMapping(value = "deleteAllInventoriesCreatedAfterDate", method = RequestMethod.POST)
+    public void deleteAllInventoriesCreatedAfterDate(Date createdDate) {
+        inventoryService.deleteAllInventoriesCreatedAfterDate(createdDate);
+    }
+
+    @RequestMapping(value = "updateInventory", method = RequestMethod.POST)
+    public void updateInventory(InventoryRequestObject inventoryRequestObject) {
+        inventoryService.updateInventory(inventoryRequestObject);
     }
 }
