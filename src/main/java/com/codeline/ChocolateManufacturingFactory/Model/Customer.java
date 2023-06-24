d1396c782c6283e7a6f97b8f62c34274a22673e5
Rawdha Alyhayia <rweedh@hotmail.com>
2023-06-24 13:03:57 +0400

    Removed Repeated Dependencies & Removed getter and setter annotation in customer

--- src/main/java/com/codeline/ChocolateManufacturingFactory/Model/Customer.java
+++ src/main/java/com/codeline/ChocolateManufacturingFactory/Model/Customer.java
@@ -1,14 +1,10 @@
 package com.codeline.ChocolateManufacturingFactory.Model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Data
-@Setter
-@Getter
 @Entity
 public class Customer extends BaseEntity {
     @Id
